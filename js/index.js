2c4b61a0ad2d81061184af9d9e124c39cc6f454f
JonathanEmanuel12 <jonathaanemanuel21@gmail.com>
2021-09-12 22:20:52 -0300

    tabuleiro pronto

--- js/index.js
+++ js/index.js
@@ -1,14 +1,30 @@
-var controle = 0;
-console.log("Eita");
-for(var i=0; i<9; i++)
+var controle = false;
+
+var gridPrincipal = document.getElementById("gridPrincipal");   
+
+for (var i=0; i<10; i++)
 {
-    if(controle == 0)
+    for(var j=0; j<10; j++)
     {
-        document.getElementById("0"+i.toString()).style.backgroundColor = "#000";
-        controle = 1;
-    } else {
-        console.log("0"+i.toString())
-        document.getElementById("0"+i.toString()).style.backgroundColor = "#fff";
-        controle = 0;
+        var divTabuleiro = document.createElement("div");
+        divTabuleiro.classList.add("grid_parte");
+        divTabuleiro.id = i.toString() + j.toString();
+        divTabuleiro.innerHTML = i.toString() + j.toString();
+        gridPrincipal.appendChild(divTabuleiro);
     }
 }
+for(var i=0; i<10; i++)
+{
+    for(var j=0; j<10; j++)
+    {
+        if(controle == false)
+        {
+            document.getElementById(i.toString() + j.toString()).style.backgroundColor = "#000";
+        } else {
+            document.getElementById(i.toString() + j.toString()).style.backgroundColor = "#fff";
+        }
+        controle = !controle;
+    }
+    controle = !controle;
+}
+
